90dbc8f6b161fa6d02951fe418d1f79aafffa230
JYlag <joeylagan@gmail.com>
2017-07-23 19:57:30 -0700

    Added Database to Heroku

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import cgi
+import os
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:blogz@localhost:8889/blogz'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI', 'mysql+pymysql://blogz:blogz@localhost:8889/blogz')
 app.config['SQLALCHEMY_ECHO'] = True
 
 db = SQLAlchemy(app)
